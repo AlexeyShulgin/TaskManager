2c9ec6b0674057b54b9db7b5f805e0b4305a4564
AlexeyShulgin <shulgin97@i.ua>
2016-05-23 09:33:46 +0300

    TaskManager
    My first task manager on C# (Windows Forms)

---
+++ TaskManager/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader13 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader14 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11,
+            this.columnHeader12,
+            this.columnHeader13,
+            this.columnHeader14});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1038, 290);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 75;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Приоритет";
+            this.columnHeader2.Width = 89;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Дескриптор";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Количество дескрипторов";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "ID процесса";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Имя ПК";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Базовый адрес загрузки";
+            this.columnHeader7.Width = 63;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Дескриптор окна";
+            this.columnHeader8.Width = 62;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Заголовок окна";
+            this.columnHeader9.Width = 79;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Количество модулей";
+            this.columnHeader10.Width = 71;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Отвечает ли интерфейс";
+            this.columnHeader11.Width = 70;
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "Время запуска процесса";
+            this.columnHeader12.Width = 76;
+            // 
+            // columnHeader13
+            // 
+            this.columnHeader13.Text = "Процессорное время";
+            this.columnHeader13.Width = 145;
+            // 
+            // columnHeader14
+            // 
+            this.columnHeader14.Text = "Количество потоков";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1038, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 320);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Дипетчер задач";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+        private System.Windows.Forms.ColumnHeader columnHeader13;
+        private System.Windows.Forms.ColumnHeader columnHeader14;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ TaskManager/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Thread t = null;
+        int n = 5000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            t = new Thread(Thr);
+            t.Start();
+        }
+        public void Thr()
+        {
+            while (true)
+            {
+                try
+                {
+                    Process[] mas_proc = Process.GetProcesses();
+                    foreach (Process proc in mas_proc)
+                    {
+                        try
+                        {
+                            this.listView1.Items.Add(new ListViewItem(new string[] { proc.ProcessName.ToString(), proc.BasePriority.ToString(), proc.Handle.ToString(), proc.HandleCount.ToString(), proc.Id.ToString(), proc.MachineName.ToString(), proc.MainModule.EntryPointAddress.ToString(), proc.MainWindowHandle.ToString(), proc.MainWindowTitle.ToString(), proc.Modules.Count.ToString(), proc.Responding.ToString(), proc.StartTime.ToString(), proc.TotalProcessorTime.ToString(), proc.Threads.Count.ToString() }));
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                    Thread.Sleep(n);
+                    this.listView1.Items.Clear();
+                }
+                catch(Exception)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if( t != null )
+                t.Abort();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "Текстовые файлы (*.txt)|*.txt";
+            if (sd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Stream stream = sd.OpenFile();
+                StreamWriter sw = new StreamWriter(stream);
+                try
+                {
+                    Process[] mas_proc = Process.GetProcesses();
+                    foreach (Process proc in mas_proc)
+                    {
+                        try
+                        {
+                            sw.WriteLine("=========={0}==========", proc.ProcessName);
+                            sw.WriteLine("Приоритет: {0}", proc.BasePriority);
+                            sw.WriteLine("Дескриптор: {0}", proc.Handle);
+                            sw.WriteLine("Количество дескрипторов: {0}", proc.HandleCount);
+                            sw.WriteLine("ID: {0}", proc.Id);
+                            sw.WriteLine("Имя ПК: {0}", proc.MachineName);
+                            sw.WriteLine("Базовый адрес загрузки: {0}", proc.MainModule.EntryPointAddress);
+                            sw.WriteLine("Дескриптор окна: {0}", proc.MainWindowHandle);
+                            sw.WriteLine("Заголовок окна: {0}", proc.MainWindowTitle);
+                            sw.WriteLine("Модулей: {0}", proc.Modules.Count);
+                            foreach (ProcessModule pm in proc.Modules)
+                            {
+                                try
+                                {
+                                    sw.WriteLine("\tИмя модуля: {0}", pm.FileName);
+                                    sw.WriteLine("\t\tАдрес модуля: {0}", pm.BaseAddress);
+                                    sw.WriteLine("\t\t\tПроизводитель модуля: {0}\r\n", pm.FileVersionInfo.CompanyName);
+                                }
+                                catch (Exception ex)
+                                {
+                                    sw.WriteLine("Ошибка {0} ({1})", ex.Message, proc.ProcessName);
+                                }
+                            }
+                            sw.WriteLine("Отвечает ли интерфейс: {0}", proc.Responding);
+                            sw.WriteLine("Время запуска процесса: {0}", proc.StartTime.ToString());
+                            sw.WriteLine("Процессорное время: {0}", proc.TotalProcessorTime.ToString());
+                            sw.WriteLine("Количество потоков: {0}", proc.Threads.Count);
+                            foreach (ProcessThread pt in proc.Threads)
+                            {
+                                try
+                                {
+                                    sw.WriteLine("\tАдрес: {0};\r\n\t\t ID: {1}\r\n", pt.StartAddress, pt.Id);
+                                }
+                                catch (Exception ex)
+                                {
+                                    sw.WriteLine("Ошибка {0} ({1})", ex.Message, proc.ProcessName);
+                                }
+                            }
+                            sw.WriteLine("\n\n");
+                        }
+                        catch (Exception ex)
+                        {
+                            sw.WriteLine("Ошибка {0} ({1})", ex.Message, proc.ProcessName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка");
+                }
+                stream.Close();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
